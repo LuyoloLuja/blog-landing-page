dc9345e037201f942791a20ed2afcdb3c47bc892
LuyoloLuja <luyolom19@gmail.com>
2021-03-15 16:42:15 +0200

    added js on nav bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
-document.querySelector('.expand-links').addEventListener('click', () => {
-    document.querySelector('.second-nav').style.display = 'block';
-})
+// DOM ELEMENTS
+const wrapper = document.querySelector(".first-nav");
+
+const navBarA = document.querySelector(".a");
+const navBarB = document.querySelector(".b");
+const navBarC = document.querySelector(".c");
+
+// FUNCTIONS
+function navFunctionality(event) {
+  const element = event.target;
+
+  // if (element.nodeName !== 'A' || !element.classList.contains('first-name')) {
+  //     return;
+  // }
+  const targetButton = element.dataset.input;
+  openNavBar(targetButton);
+}
+
+function openNavBar(input) {
+  if (input === "0") {
+    navBarA.classList.remove('hide');
+    navBarC.classList.add('hide');
+    navBarB.classList.add('hide');
+  } else if (input === "1") {
+    navBarB.classList.remove('hide');
+    navBarA.classList.add('hide');
+    navBarC.classList.add('hide');
+  } else if (input === "2") {
+    navBarC.classList.remove('hide');
+    navBarA.classList.add('hide');
+    navBarB.classList.add('hide');
+  }
+}
+
+function closeNavBar() {
+    navBarA.classList.add('hide');
+    navBarC.classList.add('hide');
+    navBarB.classList.add('hide');
+}
+
+// EVENTS
+wrapper.addEventListener("mouseover", navFunctionality);
+wrapper.addEventListener('mouseout', closeNavBar)
